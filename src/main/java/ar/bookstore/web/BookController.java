5024f13c94c64fb47f2e33ec9fdf0febbbb64248
Aleksis Rautiainen <aleraut9@gmail.com>
2023-04-06 18:18:48 +0300

    bookstore REST

--- src/main/java/ar/bookstore/web/BookController.java
+++ src/main/java/ar/bookstore/web/BookController.java
@@ -1,11 +1,15 @@
 package ar.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import ar.bookstore.domain.Book;
 import ar.bookstore.domain.BookRepository;
@@ -21,13 +25,25 @@ public class BookController {
     private CategoryRepository categoryRepository;
 
     // Show all books
-    @RequestMapping(value={"/", "/booklist"})
+    @RequestMapping(value={"/booklist"})
 	public String bookList(Model model) {
 
 		model.addAttribute("books", bookRepository.findAll());
 		model.addAttribute("categories", categoryRepository.findAll());
 
 		return "booklist";
+    }
+
+    // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+        return (List<Book>) bookRepository.findAll();
+    }
+
+    // RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+        return bookRepository.findById(bookId);
     }
 
     // Add a book
